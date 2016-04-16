6d1871f1fe0bba809edc5a198168609f53010ec3
Anton Afanasyev <fun@bk.ru>
2016-04-16 11:51:03 +0000

    Fix for supergroup chat id

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ from pony.orm import *
 lacanDB = Database()
 class Chat(lacanDB.Entity):
     primary_id = PrimaryKey(int, auto=True)
-    chat_id = Required(int, unique=True)
+    chat_id = Required(int, size=64, unique=True)
     user_id = Required(int)
     open_date = Required(datetime)
     last_message_date = Optional(datetime)
